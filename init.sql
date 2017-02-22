8a74137f8b39750fc55fa391e97f997530d96acb
Russley Shaw <russleyshaw@gmail.com>
2017-02-21 21:18:16 -0600

    Made sql more gitlab friendly

--- init.sql
+++ init.sql
@@ -3,26 +3,19 @@ CREATE TYPE pizza_choice_enum AS ENUM ('cheese', 'pepperoni', 'bacon', 'chicken'
 CREATE TYPE prog_lang_enum AS ENUM ('cpp', 'python', 'csharp', 'javascript', 'java', 'lua');
 CREATE TYPE game_status_enum as ENUM ('scheduled', 'playing', 'finished');
 
+-- extra data for GitLab User representing an individual competitor
 CREATE TABLE "user" (
-    id serial NOT NULL PRIMARY KEY,
-    gitlab_id integer NOT NULL, 
+    id integer NOT NULL PRIMARY KEY,
 
     is_eligible boolean NOT NULL DEFAULT true,
 
     shirt_size shirt_size_enum,
-    pizza_choice pizza_choice_enum,
-
-    created_time timestamp NOT NULL DEFAULT now(),
-    modified_time timestamp NOT NULL DEFAULT now()
+    pizza_choice pizza_choice_enum
 );
 
+-- extra data for GitLab Group representing a competition
 CREATE TABLE "competition" (
-    id serial NOT NULL PRIMARY KEY,
-    name varchar(64) UNIQUE,
-    description varchar(128) NOT NULL DEFAULT 'Competition description here.',
-
-    slogan varchar(64) NOT NULL DEFAULT 'Competition slogan here.',
-    logo bytea,
+    id number NOT NULL PRIMARY KEY,
 
     start_time timestamp,
     end_time timestamp,
@@ -32,33 +25,18 @@ CREATE TABLE "competition" (
 
     min_team_size integer NOT NULL DEFAULT 2,
     max_team_size integer NOT NULL DEFAULT 2,
-
-    created_time timestamp NOT NULL DEFAULT now(),
-    modified_time timestamp NOT NULL DEFAULT now()
 );
 
+-- extra data for GitLab Project representing a team
 CREATE TABLE "team" (
-    id serial NOT NULL PRIMARY KEY,
-    gitlab_id integer NOT NULL,
-
-    created_time timestamp NOT NULL DEFAULT now(),
-    modified_time timestamp NOT NULL DEFAULT now()
-);
-
-CREATE TABLE "user_team" (
-    user_id integer NOT NULL REFERENCES "user",
-    team_id integer NOT NULL REFERENCES "team",
-    PRIMARY KEY(user_id, team_id)
+    id integer NOT NULL PRIMARY KEY
 );
 
 CREATE TABLE "invite" (
     id serial NOT NULL PRIMARY KEY,
     team integer NOT NULL REFERENCES "team",
     member integer NOT NULL REFERENCES "user",
-    message varchar(140) NOT NULL DEFAULT ' ',
-
-    created_time timestamp NOT NULL DEFAULT now(),
-    modified_time timestamp NOT NULL DEFAULT now()
+    message varchar(140) NOT NULL DEFAULT ' '
 );
 
 -- An individual game between teams
@@ -66,6 +44,7 @@ CREATE TABLE "game" (
     id serial NOT NULL PRIMARY KEY,
     status game_status_enum NOT NULL DEFAULT 'scheduled',
 
+    winner integer,
     win_reason varchar(64),
     lose_reason varchar(64),
 
@@ -76,12 +55,4 @@ CREATE TABLE "game" (
 
     created_time timestamp NOT NULL DEFAULT now(),
     modified_time timestamp NOT NULL DEFAULT now()
-);
-
--- Teams participating in a game
-CREATE TABLE "team_game" (
-    winner boolean,
-    team_id integer NOT NULL REFERENCES "team",
-    game_id integer NOT NULL REFERENCES "game",
-    PRIMARY KEY(team_id, game_id)
 );
