1a65dc8409463e9bfe050974d0e03a72a960a1b0
user404d <conduffql@gmail.com>
2017-03-07 03:03:13 -0600

    Fix testing script and travis build

--- test.sh
+++ test.sh
@@ -5,16 +5,18 @@ spin="-\|/";
 dots=". . . . . . . . . . ";
 i=0
 
-while ! docker exec "$@" psql postgres -U postgres -h localhost -c 'select * from competition' &>/dev/null; do
+while ! docker exec "$@" psql -U postgres -h localhost \
+      -c "create database docker" &>/dev/null; do
   i=$(( (i+1) % 4));
-  printf "\rWaiting for postgres ${spin:$i:1} ${dots:0:(($timeout*2))}";
+  printf "\rWaiting for postgres ${spin:$i:1} ${dots:0:(($timeout * 2))}";
   sleep 1;
   timeout=$((timeout + 1));
   if ((timeout==10)); then
-    echo -e "\nFailed to build database!";
+    echo -e "\nDatabase failed to start!";
     exit 1;
   fi;
 done;
 
-echo -e "\nSuccessfully built database!";
-exit 0;
+echo -e "\nDatabase successfully started!";
+
+docker exec "$@" psql docker -v ON_ERROR_STOP=1 -U postgres -h localhost -f /tmp/init.sql
