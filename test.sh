24df2e26ceb5be0f6d15746cee0728402e5f1843
user404d <conduffql@gmail.com>
2017-03-07 05:02:44 -0600

    Fix unstable connection

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ spin="-\|/";
 dots=". . . . . . . . . . ";
 i=0
 
-while ! docker exec "$1" psql -U postgres -h localhost \
+while ! docker exec "$1" psql postgres postgres\
       -c "create database docker" &>/dev/null; do
   i=$(( (i+1) % 4));
   printf "\rWaiting for postgres ${dots:0:(($timeout * 2))} ${spin:$i:1}";
@@ -20,5 +20,21 @@ done;
 
 printf "\r\e[KWaiting for postgres ${dots:0:(($timeout * 2))} \xe2\x9c\x94";
 echo -e "\nDatabase successfully started!";
+
+while ! docker exec "$1" psql docker postgres\
+      -c "select 1" &>/dev/null; do
+  i=$(( (i+1) % 4));
+  printf "\rWaiting for stable connection ${dots:0:(($timeout * 2))} ${spin:$i:1}";
+  sleep 1;
+  timeout=$((timeout + 1));
+  if ((timeout==10)); then
+    printf "\r\e[KWaiting for stable connection ${dots:0:(($timeout * 2))} \xe2\x9c\x92";
+    echo -e "\nStable connection failed!";
+    exit 1;
+  fi;
+done;
+
+printf "\r\e[KWaiting for stable connection ${dots:0:(($timeout * 2))} \xe2\x9c\x94";
+echo -e "\nStable connection succeeded!";
 
 docker exec "$1" psql -v ON_ERROR_STOP="$2" -f /tmp/init.sql docker postgres;
