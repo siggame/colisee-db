0397b90ed6e9c98d9dcfbe2b9d9d3d50a7c97a0e
Russley Shaw <russleyshaw@gmail.com>
2017-04-13 13:39:01 -0500

    Update init.sql

--- init.sql
+++ init.sql
@@ -1,11 +1,12 @@
 CREATE TYPE shirt_size_enum AS ENUM ('s', 'm', 'l', 'xl', 'xxl');
 CREATE TYPE pizza_choice_enum AS ENUM ('cheese', 'pepperoni', 'bacon', 'chicken');
-CREATE TYPE prog_lang_enum AS ENUM ('cpp', 'python', 'csharp', 'javascript', 'java', 'lua');
 CREATE TYPE game_status_enum as ENUM ('scheduled', 'playing', 'finished');
 
 -- extra data for GitLab User representing an individual competitor
 CREATE TABLE "user" (
-    id integer NOT NULL PRIMARY KEY,
+    id serial NOT NULL PRIMARY KEY,
+    name varchar(32) NOT NULL UNIQUE,
+    full_name varchar(128),
 
     is_eligible boolean NOT NULL DEFAULT true,
 
@@ -13,30 +14,21 @@ CREATE TABLE "user" (
     pizza_choice pizza_choice_enum
 );
 
--- extra data for GitLab Group representing a competition
-CREATE TABLE "competition" (
-    id integer NOT NULL PRIMARY KEY,
-
-    start_time timestamp,
-    end_time timestamp,
-
-    is_open boolean NOT NULL DEFAULT false,
-    is_running boolean NOT NULL DEFAULT false,
-
-    min_team_size integer NOT NULL DEFAULT 2,
-    max_team_size integer NOT NULL DEFAULT 2
+CREATE TABLE "team_member" (
+       
 );
 
 -- extra data for GitLab Project representing a team
 CREATE TABLE "team" (
-    id integer NOT NULL PRIMARY KEY
-);
-
-CREATE TABLE "invite" (
     id serial NOT NULL PRIMARY KEY,
-    team integer NOT NULL REFERENCES "team",
-    member integer NOT NULL REFERENCES "user",
-    message varchar(140) NOT NULL DEFAULT ' '
+    
+    latest_submission_tar_url varchar(64),
+    latest_submission_build_output_url varchar(64),
+    latest_submission_time timestamp,
+    
+    valid_submission_tar_url varchar(64),
+    valid_submission_build_output_url varchar(64),
+    valid_submission_time timestamp,
 );
 
 -- An individual game between teams
@@ -44,11 +36,10 @@ CREATE TABLE "game" (
     id serial NOT NULL PRIMARY KEY,
     status game_status_enum NOT NULL DEFAULT 'scheduled',
 
-    winner integer,
     win_reason varchar(64),
     lose_reason varchar(64),
 
-    gamelog bytea,
+    gamelog_url varchar(64),
 
     -- if this game has been visualized before
     visualized boolean NOT NULL DEFAULT false,
